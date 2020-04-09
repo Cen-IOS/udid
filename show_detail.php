c0acb9b3656658b216cd42b1c02b338f47ebc5d4
Cen-IOS <37799940+Cen-IOS@users.noreply.github.com>
2020-04-09 13:14:25 +0200

    Add files via upload

--- show_detail.php
+++ show_detail.php
@@ -1,4 +1,62 @@
-<?php 
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="CenIOS - The VIP service developed by Cen that helps you solve certificate revocation problems">
+		<!-- Fav Icon  -->
+		<link rel="shortcut icon" href="images/favicon.ico">
+		<!-- Site Title  -->
+		<title>Cen IOS - Pubg No JailBreal Hack The Vip Version</title>
+		<!-- Vendor Bundle CSS -->
+		<link rel="stylesheet" href="assets/css/vendor.bundle.css" >
+		<!-- Custom styles for this template -->
+		<link href="assets/css/style.css?ver=1.2" rel="stylesheet">
+		<link href="assets/css/theme.css?ver=1.2" rel="stylesheet" id="layoutstyle">
+		<!-- Site Meta  -->
+                <meta property="og:image" content="images/hs-2.png" />
+                <meta property="og:url" content="https://cen-ios.github.io/Cenios/" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="CenIOS - Pubg Mobile Hack Service - NonJB" />
+                <meta property="og:description" content="CenIOS - Pubg Mobie Hack Service Good to help you overcome difficulties with idols" />
+
+	</head>
+	<body class="overflow-scroll">
+       	
+		<!-- Start .header-section -->
+		<div id="home" class="header-section flex-box-middle section gradiant-background overflow-hidden">
+			<div id="particles-js" class="particles-container"></div>
+			<div id="navigation" class="navigation is-transparent" data-spy="affix" data-offset-top="5">
+				<nav class="navbar navbar-default">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#site-collapse-nav" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+					</div><!-- /.container -->
+				</nav>
+			</div><!-- .navigation -->
+			
+			<div class="header-content">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="header-texts">
+								<h1 class="cd-headline clip is-full-width wow fadeInUp" data-wow-duration=".5s">
+									<span>Cen IOS</span> 
+									<span class="cd-words-wrapper">
+										<b class="is-visible">Pubg Hack No-Jb</b>
+										<b>Pubg Hack JB</b>
+										
+									</span>
+								</h1>
+								
+								<?php 
     $subject = "This is my UDID from iOS device";
     $body  = "Hello<br /> This is my UDID: {$_GET['UDID']} <br />";
     $body .= "Device product: {$_GET['DEVICE_PRODUCT']} <br />";
@@ -43,3 +101,60 @@ function myCopyFunction() {
   document.body.removeChild(myText);
 }
 </script>
+							</div>
+						</div><!-- .col -->
+						<div class="col-md-6 header-mockup">
+                                                 
+						</div><!-- .col -->
+					</div><!-- .row -->
+				</div><!-- .container -->
+			</div><!-- .header-content -->	
+		</div><!-- .header-section -->
+		
+		
+		<!-- Start .feature-boxes  -->
+		<div class="feature-boxes">
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-md-3 col-sm-6">
+						<div class="box wow fadeInUp" data-wow-duration=".5s">
+							<div class="box-icon">
+								<em class="ti ti-user"></em>
+							</div>
+							<h4>Enthusiastic support</h4>
+						</div>
+					</div><!-- .col -->
+					<div class="col-md-3 col-sm-6">
+						<div class="box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".3s">
+							<div class="box-icon">
+								<em class="ti ti-bolt"></em>
+							</div>
+							<h4>Download Pubg Hack With Altar Speed</h4>
+						</div>
+					</div><!-- .col -->
+					<div class="col-md-3 col-sm-6">
+						<div class="box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".6s">
+							<div class="box-icon">
+								<em class="ti ti-cup"></em>
+							</div>
+							<h4>Extremely Good Quality</h4>
+						</div>
+					</div><!-- .col -->
+					<div class="col-md-3 col-sm-6">
+						<div class="box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".9s">
+							<div class="box-icon">
+								<em class="ti ti-gift"></em>
+							</div>
+							<h4>Weighing Scales Steal The Steal File For Sale</h4>
+						</div>
+					</div><!-- .col -->
+				</div><!-- .row -->
+			</div><!-- .container -->
+		</div><!-- .header-features -->
+		
+		
+		<script src="assets/js/jquery.bundle.js"></script>
+		<script src="assets/js/script.js"></script>
+		
+	</body>
+</html>
